e36cd42b7581d8ea0ac777b2980ffb91eb922ef8
Jordi Gou <jordi.gou@asg.com>
2020-08-28 12:28:07 +0200

    Header navigation bar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent implements OnInit {
-
+  @Output() currentHeaderOption = new EventEmitter<string>();
+  
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  OnChangeOption(option: string){
+    this.currentHeaderOption.emit(option);
+  }
 }
